c813fcd6dda2b1f6c7029794cfd7068b105724c0
Paul Georgiou <pavlos1998@gmail.com>
2018-02-21 00:35:55 +0000

    Improve Title Generator

--- titlegen.py
+++ titlegen.py
@@ -1,5 +1,5 @@
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile
 
 import markovify, io
 
@@ -7,17 +7,17 @@ def getTextFiles():
     return [f for f in listdir('.') if isfile(f) and f.endswith('.txt')]
 
 class TitleGenerator:
-    _textModel = None
-
-    def __init__(self):
-        corpus = ''
+    def __init__(self, tlen=200):
+        self.titleLength = tlen
+        models = []
         for fname in getTextFiles():
+            dots = fname.split('.')
             with io.open(fname, 'r', encoding='utf-8') as f:
-                corpus += f.read()
-        self._textModel = markovify.NewlineText(corpus)
+                models.append((markovify.NewlineText(f.read(), state_size=2), int(dots[-2]) if len(dots) > 2 else 100))
+        self.textModel = markovify.combine([a for a, b in models], [b for a, b in models])
 
     def generateTitle(self):
-        return self._textModel.make_sentence(tries=100)
+        return self.textModel.make_short_sentence(self.titleLength, tries=100, max_overlap_ratio=0.6)
 
 if __name__ == '__main__':
     gen = TitleGenerator()
