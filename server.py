fa9a753aa3466130ae513fd3095c8701907e8e3e
Paul Georgiou <pavlos1998@gmail.com>
2018-02-21 00:39:55 +0000

    Add Webserver

---
+++ server.py
@@ -0,0 +1,50 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import path
+from titlegen import TitleGenerator
+import json, io
+
+WEB_ROOT = 'static'
+
+TGEN = None
+
+class TheGarlicServer(BaseHTTPRequestHandler):
+    mimeTypes = {
+        'txt': 'text/plain',
+        'html': 'text/html',
+        'css': 'text/css',
+        'js': 'application/javascript',
+        'png': 'image/png',
+        'jpeg': 'image/jpeg'
+    }
+
+    def do_GET(self):
+        if self.path.startswith('/api/post.json'):
+            titles = dict(titles=[TGEN.generateTitle() for _ in range(5)])
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(titles).encode('utf-8'))
+        else:
+            filepath = self.path
+            if filepath.endswith('/'):
+                filepath += 'index.html'
+            filepath = WEB_ROOT + path.join(*('.' + filepath).split('/'))[1:]
+            try:
+                ext = filepath.split('.')[-1]
+                with io.open(filepath, 'rb') as f:
+                    self.send_response(200)
+                    self.send_header('Content-Type', self.mimeTypes.get(ext, 'application/octet-stream'))
+                    self.end_headers()
+                    self.wfile.write(f.read())
+            except Exception as e:
+                self.send_response(404)
+                self.send_header('Content-Type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(('404 ' + self.path).encode('utf-8'))
+
+if __name__ == '__main__':
+    TGEN = TitleGenerator(150)
+    server_address = ('127.0.0.1', 9420)
+    httpd = HTTPServer(server_address, TheGarlicServer)
+    print('running server...')
+    httpd.serve_forever()
